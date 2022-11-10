900fc997b456167f0973414a1c51052c813b3f2d
JP <joao.santos-ctr@vectorsolutions.com>
2022-11-10 01:59:07 -0300

    create transaction and install file interceptor

---
+++ src/backend/transaction/transaction.controller.ts
@@ -0,0 +1,16 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('transaction')
+export class TransactionController {
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+  }
+}
