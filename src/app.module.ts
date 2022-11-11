c807fd9b9dd67be88cb3d2729436288ba6ba0d9a
JP <joao.santos-ctr@vectorsolutions.com>
2022-11-11 11:54:54 -0300

    add env file to set env variables up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BackendModule } from './backend/backend.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BackendModule],
+  imports: [ConfigModule.forRoot(), BackendModule],
   controllers: [AppController],
   providers: [AppService],
 })
